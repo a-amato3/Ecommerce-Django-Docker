3fa33020ac66cf9b8d87fe483abc32b6bdb6595c
Aaron Amato <aaronamato@hotmail.com>
2020-09-15 13:45:44 +0200

    added add to cart logic

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,34 +1,78 @@
 from django.shortcuts import render
-from .models import * 
+from .models import *
+from django.http import JsonResponse
+import json
+
 
 def store(request):
-	products = Product.objects.all()
-	context = {'products':products}
-	return render(request, 'store/store.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
+
 
 def cart(request):
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-	else:
-		#Create empty cart for now for non-logged in user
-		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
-  
-	context = {'items':items, 'order':order}
-	return render(request, 'store/cart.html', context)
-
-def checkout(request):
-    
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
         item = []
-        order = {'get_cart_total':0, 'order':order}
-        
-    context = {'items':items, 'order':order}
+        order = {'get_cart_total': 0, 'order': order}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(
+        order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem = (orderItem.quantity - 1)
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
